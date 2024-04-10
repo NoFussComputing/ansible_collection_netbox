7f7a9bdcf8add493ac9dca9145a38ccc00edad13
Jon <jonathon.lockwood@networkedweb.com>
2024-04-10 17:58:24 +0930

    ci: as part of release, update default vars image tag to current release
    !8

--- .gitlab/additional_actions_bump.sh
+++ .gitlab/additional_actions_bump.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-echo " no additional actions in repo";
+sed -E "/tag: /s/[0-9]+\.[0-9]+\.[0-9]+[\-]?[a-z]?[0-9]+?/$(cz version --project)/" -i roles/netbox_glpi/defaults/main.yaml;
+
+git add roles/netbox_glpi/defaults/main.yaml;
+
+git status;
